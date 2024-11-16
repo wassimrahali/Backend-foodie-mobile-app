d71b31901cccd89f4046f65722d16efdeeb8dc5b
Mehdi Ben Fakhta <mehdi.benfakhta@gmail.com>
2024-11-16 20:55:55 +0100

    Update update-order.ts

--- src/api/routes/orders/controllers/update-order.ts
+++ src/api/routes/orders/controllers/update-order.ts
@@ -3,28 +3,17 @@ import { Request, Response } from "express"
 export async function updateOrder(req: Request, res: Response) {
     try {
         const { id } = req.params;
-        const { totalPrice, status, location, customerId, deliveryManId, orderItems } = req.body;
+        const { status, deliveryManId } = req.body;
 
         const updatedOrder = await prisma.order.update({
             where: { id: parseInt(id) },
             data: {
-                totalPrice,
                 status,
-                location,
-                customer: customerId ? { connect: { id: customerId } } : undefined,
                 deliveryMan: deliveryManId ? { connect: { id: deliveryManId } } : undefined,
-                orderItems: orderItems?.length > 0
-                    ? {
-                        deleteMany: {}, // Clears existing order items for replacement
-                        create: orderItems.map((item: any) => ({
-                            quantity: item.quantity,
-                            product: { connect: { id: item.productId } },
-                        })),
-                    }
-                    : undefined,
             },
             include: { orderItems: true, customer: true, deliveryMan: true },
         });
+
         res.json(updatedOrder);
     } catch (error) {
         res.status(500).json({ error: error.message });
