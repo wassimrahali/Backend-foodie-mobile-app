d28855416cc6d6f07dea76381bfd41610a5f5201
Chiheb13 <Yahyachiheb8@gmail.com>
2024-11-19 12:04:25 -1200

    Fix TypeScript setup for production and development

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import app from "./app"
-const port = process.env.PORT || 5000
-
+const port = process.env.PORT || 8000; // Fallback to 8000 if no environment variable is set
 app.listen(port, () => {
-    console.log(`Listening: http://localhost:${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
 
