97ff627e16ff18fd1915d283ee728e8a393a07b3
your name <your.name@domain.com>
2024-11-20 14:25:34 +0100

    change in update func

--- src/api/routes/auth/DileveryMan/controllers/update-DileveryMan.ts
+++ src/api/routes/auth/DileveryMan/controllers/update-DileveryMan.ts
@@ -1,17 +1,27 @@
-import { Request, Response } from "express"
-import { prisma } from "@root/prisma/prisma"
+import { Request, Response } from "express";
+import { prisma } from "@root/prisma/prisma";
+import bcrypt from "bcrypt";
+
 export async function updateDeliveryMan(req: Request, res: Response) {
     try {
         const id = Number(req.params.id);
         if (!id) {
             throw new Error("Id must be a number");
         }
+
+        if (req.body.password) {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10); 
+            req.body.password = hashedPassword;  
+        }
+
         const updatedDeliveryMan = await prisma.dileveryMan.update({
             where: { id },
             data: req.body,
         });
+
         return res.send(updatedDeliveryMan);
     } catch (error) {
-        throw new Error(error);
+        console.error(error);  
+        return res.status(500).send({ message: "An error occurred while updating the delivery man." });
     }
 }
