36e2654c84242c73c36944172801da681e7838ab
Mehdi Ben Fakhta <mehdi.benfakhta@gmail.com>
2024-11-23 19:44:04 +0100

    Update router.ts

--- src/api/routes/orders/router.ts
+++ src/api/routes/orders/router.ts
@@ -9,6 +9,7 @@ import { getOrderUserId } from "./controllers/getOrderUserId";
 import { updateOrderStatus } from "./controllers/update-order-status";
 import { getOrdersByStatus } from "./controllers/get-order-by-status";
 import { getOrdersByDeliveryManId } from "./controllers/get-ordre-By-deliveryManId";
+
 const orderRouter = express.Router();
 
 orderRouter.get("/orders", getAllOrders);
@@ -21,4 +22,10 @@ orderRouter.get("/orders/customer/:customerId", getOrderUserId);
 orderRouter.patch("/orders/update-status/:id", updateOrderStatus);
 orderRouter.get("/orders/status/:status", getOrdersByStatus); 
 orderRouter.get("/orders/deliveryMan/:deliveryManId", getOrdersByDeliveryManId);
+
+// Gestion des routes non trouvées
+orderRouter.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
 export default orderRouter;
