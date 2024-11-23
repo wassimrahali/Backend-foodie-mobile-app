168098fe8e49a3363ea5879b2204bddd1596a3db
Mehdi Ben Fakhta <mehdi.benfakhta@gmail.com>
2024-11-23 19:13:37 +0100

    Update get-ordre-By-deliveryManId.dart

--- src/api/routes/orders/controllers/get-ordre-By-deliveryManId.dart
+++ src/api/routes/orders/controllers/get-ordre-By-deliveryManId.dart
@@ -1,1 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
 
+const prisma = new PrismaClient();
+
+export const getOrdersByDeliveryManId = async (req: Request, res: Response) => {
+  const { deliveryManId } = req.params;
+
+  if (!deliveryManId) {
+    return res.status(400).json({ error: "DeliveryMan ID is required" });
+  }
+
+  try {
+    const orders = await prisma.order.findMany({
+      where: {
+        deliveryManId: parseInt(deliveryManId), // Assurez-vous que deliveryManId est un entier
+      },
+      include: {
+        customer: true, // Inclut les détails du client
+        deliveryMan: true, // Inclut les détails du livreur
+        orderItems: {
+          include: {
+            product: true, // Inclut les détails des produits dans les items de commande
+          },
+        },
+      },
+    });
+
+    res.json(orders);
+  } catch (error) {
+    console.error("Error fetching orders by deliveryManId:", error);
+    res.status(500).json({ error: "Error fetching orders by deliveryManId" });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
